d5d4e197a429da8f595685427b8cef3ee9fb7554
ronnygeo <ronnygeomatt@gmail.com>
2016-01-27 01:51:28 -0500

    home page initial commit in experiments.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 #! bin/env node
 var express = require('express');
 var app = express();
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.use('/',express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 //Default route
 app.get('/', function(req, res){
