25d049b8e20e5f8765f71f07933b543ba8f337c1
ronnygeo <ronnygeomatt@gmail.com>
2016-03-04 16:11:53 +0530

    Working on the admin section.

--- public/assignment/views/admin/admin.controller.js
+++ public/assignment/views/admin/admin.controller.js
@@ -6,7 +6,37 @@
     angular.module("FormBuilderApp")
         .controller("AdminController", AdminController);
 
-    function AdminController($scope){
+    AdminController.$inject = ['$scope', 'UserService'];
+    function AdminController($scope, UserService){
+        $scope.user = {};
+        //Event Handlers
+        $scope.edit = edit;
+        $scope.remove = remove;
+        $scope.add = add;
+        $scope.update = update;
 
+        UserService.findAllUsers(render);
+        function render(data){
+            $scope.users = data;
+        }
+
+        function add() {
+            UserService.createUser($scope.user, render);
+            $scope.user = {};
+        }
+
+        function update() {}
+
+        //Add to the edit boxes.
+        function edit(index){
+            $scope.user = $scope.users[index];
+            $scope.user = {};
+        }
+
+        function remove(index){
+            $scope.user = $scope.users[index];
+            UserService.deleteUserById($scope.user._id, render)
+            $scope.user = {};
+        }
         }
 })();
--- public/assignment/views/header/header.controller.js
+++ public/assignment/views/header/header.controller.js
@@ -6,13 +6,23 @@
     angular.module('FormBuilderApp')
         .controller('HeaderController', HeaderController);
 
-    function HeaderController($scope, $location){
+    HeaderController.$inject = ['$scope', '$rootScope', '$location'];
+    function HeaderController($scope, $rootScope, $location){
+        //Event Handlers
+        $scope.logout = logout;
+
         $scope.$location = $location;
         $scope.isActive = function(loc){
             if ($location.url == loc)
                 return 'active';
         };
 
-        };
+        function logout(){
+            delete $rootScope.user;
+            console.log($rootScope.user);
+            $location.url('/');
+        }
+
+        }
 
 })();
